b422ad199129d0947486f91d3ab000915ad0da15
Julia Arrington <jarringt@uccs.edu>
2018-12-17 10:36:21 -0700

    Plan for project and all state sizes

---
+++ StateSizes/StateSizes/statesizes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using std::cout;
+using std::cin; 
+int main(void) {
+	//land area of all states
+	int alaska = 571951, texas = 261797, california = 155959, montana = 145552, newmexico = 121355, arizona = 113634, nevada = 109825,
+		colorado = 103717, wyoming = 97100, oregon = 95996, idaho = 82747, utah = 82143, kansas = 81814, minnesota = 79610,
+		nebraska = 76872, sdakota = 75884, ndakota = 68975, missouri = 68885, oklahoma = 68667, washington = 66544, georgia = 57906,
+		michigan = 56803, iowa = 55869, illinois = 55583, wisconsin = 54310, florida = 53926, arkansas = 52068, alabama = 50744,
+		ncarolina = 48710, newyork = 47213, mississippi = 46906, pennsylvania = 44816, louisiana = 43561, tennessee = 41217,
+		ohio = 40948, kentucky = 39728, virginia = 39594, indiana = 35866, maine = 30861, scarolina = 30109, wvirginia = 24077,
+		maryland = 9773, vermont = 9249, nhanpshire = 8968, massachusetts = 7840, newjersey = 7417, hawaii = 6422, connecticut = 4844,
+		delaware = 1953, rhodeisland = 1044;
+	//create a linked list
+	//create a greedy algorithm
+	//make one where states can be in multiple (3 texases and one newjersey)
+	//make another where each state is unique (texas, florida, connecticut)
+	//state how many sq miles are left over
+	//create an introduction
+}
